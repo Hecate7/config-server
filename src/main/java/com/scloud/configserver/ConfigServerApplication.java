0785af9ff8bc6b5dbb401be287b4dc84765e0ceb
Hecate <liu971006@outlook.com>
2019-04-25 17:27:40 +0800

    Initial commit

---
+++ src/main/java/com/scloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.scloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @EnableConfigServer 开启spring Cloud Config的服务端功能
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
